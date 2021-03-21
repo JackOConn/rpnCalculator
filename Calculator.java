3eaad1a8f590e035d353a2e29c0d323d3cf58522
JackOConn <47901485+JackOConn@users.noreply.github.com>
2021-03-21 01:35:01 -0400

    Added size() and clear() methods, added several tests for execute() along with the code to make them work.

--- Calculator.java
+++ Calculator.java
@@ -3,15 +3,18 @@ import java.util.*;
 public class Calculator {
     private Stack<String> stack;
     private int wordSize;
+    private int max;
 
     public Calculator() {
         this.stack = new Stack<String>();
         this.wordSize = 8;
+        this.max = 255;
     }
 
     public Calculator(int wordSize) {
         this.stack = new Stack<String>();
         this.wordSize = wordSize;
+        this.max = (int) Math.pow(2.0, Double.valueOf(wordSize)) - 1;
     }
 
     public void push(String s) {
@@ -26,13 +29,64 @@ public class Calculator {
         return stack.get(index);
     }
 
+    public int size() {
+        return stack.size();
+    }
+
+    public void clear() {
+        stack.clear();
+    }
+
     public void execute(String operation) {
-        int secondValue = Integer.parseInt(stack.pop());
-        int firstValue = Integer.parseInt(stack.pop());
+        int firstValue = 0;
+        int secondValue = 0;
+
+        if (operation.equals("+") || operation.equals("-") || operation.equals("*") || operation.equals("/")
+                || operation.equals("%")) {
+            try {
+                secondValue = Integer.parseInt(stack.pop());
+                firstValue = Integer.parseInt(stack.pop());
+            } catch (Exception e) {
+                stack.clear();
+                stack.push("ERROR");
+                return;
+            }
+        }
 
         switch (operation) {
         case "+":
             stack.push(String.valueOf(firstValue + secondValue));
+            break;
+
+        case "-":
+            stack.push(String.valueOf(firstValue - secondValue));
+            break;
+
+        case "*":
+            stack.push(String.valueOf(firstValue * secondValue));
+            break;
+
+        case "/":
+            if (secondValue == 0) {
+                stack.clear();
+                stack.push("UNDEFINED");
+            } else {
+                stack.push(String.valueOf(firstValue / secondValue));
+            }
+            break;
+
+        case "%":
+            stack.push(String.valueOf(firstValue % secondValue));
+            break;
+
+        case "clear":
+        case "c":
+            stack.clear();
+            break;
+
+        case "size":
+        case "s":
+            stack.push(String.valueOf(stack.size()));
             break;
         }
 
--- TestClass.java
+++ TestClass.java
@@ -15,6 +15,37 @@ public class TestClass {
     }
 
     @Test
+    public void PeekTest() {
+        Calculator calc = engine(8);
+        calc.push("2");
+        calc.push("3");
+        calc.push("4");
+        assertEquals("3", calc.peek(1));
+
+    }
+
+    @Test
+    public void SizeTest() {
+        Calculator calc = engine(8);
+        calc.push("2");
+        calc.push("3");
+        calc.push("4");
+        assertEquals(3, calc.size());
+
+    }
+
+    @Test
+    public void ClearTest() {
+        Calculator calc = engine(8);
+        calc.push("2");
+        calc.push("3");
+        calc.push("4");
+        calc.clear();
+        assertEquals(0, calc.size());
+
+    }
+
+    @Test
     public void AddingTest() {
         Calculator calc = engine(8);
         calc.push("5");
@@ -24,12 +55,87 @@ public class TestClass {
     }
 
     @Test
-    public void PeekTest() {
+    public void SubtractionTest() {
+        Calculator calc = engine(8);
+        calc.push("12");
+        calc.push("5");
+        calc.execute("-");
+        assertEquals("7", calc.pop());
+    }
+
+    @Test
+    public void MultiplyTest() {
+        Calculator calc = engine(8);
+        calc.push("10");
+        calc.push("5");
+        calc.execute("*");
+        assertEquals("50", calc.pop());
+    }
+
+    @Test
+    public void DivideTest() {
+        Calculator calc = engine(8);
+        calc.push("20");
+        calc.push("5");
+        calc.execute("/");
+        assertEquals("4", calc.pop());
+    }
+
+    @Test
+    public void ModulusTest() {
+        Calculator calc = engine(8);
+        calc.push("12");
+        calc.push("5");
+        calc.execute("%");
+        assertEquals("2", calc.pop());
+    }
+
+    @Test
+    public void ExecuteClear() {
         Calculator calc = engine(8);
         calc.push("2");
         calc.push("3");
         calc.push("4");
-        assertEquals("3", calc.peek(1));
+        calc.execute("clear");
+        assertEquals(0, calc.size());
+    }
 
+    @Test
+    public void ExecuteSize() {
+        Calculator calc = engine(8);
+        calc.push("2");
+        calc.push("3");
+        calc.push("4");
+        calc.execute("size");
+        assertEquals("3", calc.pop());
+    }
+
+    @Test
+    public void InvalidExecuteCall() {
+        Calculator calc = engine(8);
+        calc.push("2");
+        calc.execute("+");
+        assertEquals("ERROR", calc.peek(0));
+    }
+
+    @Test
+    public void TooManyOperators() {
+        Calculator calc = engine(8);
+        calc.push("2");
+        calc.push("2");
+        calc.execute("+");
+        calc.execute("*");
+        calc.execute("/");
+        calc.execute("-");
+        assertEquals("ERROR", calc.peek(0));
+    }
+
+    @Test
+    public void DivideByZero() {
+        Calculator calc = engine(8);
+        calc.push("1");
+        calc.push("0");
+        calc.execute("/");
+        assertEquals("ERROR", calc.peek(0));
     }
 }
